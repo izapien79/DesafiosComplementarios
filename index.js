7430220af0fc676d6ee43abd9796b6b1331dcdd2
izapien <izapien79@hotmail.com>
2022-08-30 22:50:39 -0500

    subiendo Desafio 01 Complementario

---
+++ index.js
@@ -0,0 +1,35 @@
+// FOR
+/*     const COMPRAS = prompt("Ingresa el producto a comprar: \n 1.BOLSA \n 2.LAPIZ \n 3.PAPEL")
+    const NUMERO = prompt("¿Cuantas compraras?")
+    let stock = 35
+    let resta = 0
+
+    for(let index = 1; index <= NUMERO; index++){
+        resta = stock - NUMERO
+        console.log("Me queda un stock de "+ resta);
+
+        const RESTA = 35
+        if(RESTA => 35){
+            console.log("Todavia hay stock "+ resta)
+            break;
+        } else {
+            console.log("Ya no hay piezas del producto que quieres comprar")
+        }
+    } */
+
+// ejemplo 2 FOR
+/* let nombre = "Ianira"
+for (let saludo = 1; saludo <= 20; saludo++){
+    console.log("Mi nombre es: "+ nombre+" "+saludo)
+} */
+
+// ejemplo WHILE
+/* let usuario = prompt("Ingresa tu Login")
+let password = prompt("Ingresa tu contraseña")
+
+while (password != 4321){
+    alert("Contraseña Inválida")
+    usuario = prompt("Ingresa tu Login")
+    password = prompt("Ingresa tu contraseña")
+} */
+
